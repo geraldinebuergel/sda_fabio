af0791500271b2f4ae0e17bb9685d2dc07a4e2c8
geraldinebuergel <geraldine.buergel@gmx.de>
2019-02-07 17:13:28 +0100

    reduce data to sample size & use inverses for matrix division

--- sda.R
+++ sda.R
@@ -1,66 +1,77 @@
 library(tidyverse)
+library(MASS)
+require(MASS)
+require(dplyr)
 memory.limit()
-memory.limit(60000)
+memory.limit(8000)
 
 rm(list = ls()); gc()
 
-###########################
+#------------------------------------
 # Built F = ULY
-###########################
+#------------------------------------
 
 load("C:/Users/Zoe/Desktop/FABIO/1986_E.RData")
 load("C:/Users/Zoe/Desktop/FABIO/1986_X.RData")
 load("C:/Users/Zoe/Desktop/FABIO/1986_L.RData")
 load("C:/Users/Zoe/Desktop/FABIO/1986_Y.RData")
 
--------------------------------
-  # built U in ha/1000t
--------------------------------
+#--------------------------------------------------
+# reduce data to sample size of 10 countries
+#--------------------------------------------------
+str(E)
+str(X)
+str(L)
+str(Y)
+E <- E[1:1300,]
+X <- X[1:1300]
+L <- L[1:1300, 1:1300]
+Y <- Y[1:1300, 1:40]
+
+#-------------------------------
+# built U in ha/1000t
+#-------------------------------
   
 u <- E$Landuse / X
 u[is.nan(u)] <- 0
 U <- diag(u)
 
--------------------------------
+#-------------------------------
 # built L variables
--------------------------------
-  
+#-------------------------------
+
 # level of total input requirements (Ljs)
 str(L)
 llev <- colSums(L)
 
 # distribution of supplier countries (Ljrs/Ljs)
-ljrs_list <- split.data.frame(as.data.frame(L), rep(1:192, each = 130))
+ljrs_list <- split.data.frame(as.data.frame(L), rep(1:10, each = 130))
 ljrs_list_sum <- lapply(ljrs_list, FUN = colSums)
 ljrs <- do.call(rbind, ljrs_list_sum)
 as.matrix(ljrs)
-lsup <- sweep(ljrs, 2, llev, FUN = '/')
+lsup <- ljrs %*% t(ginv(llev))
 lsup[is.nan(lsup)] <- 0
 
 # distribution of intermediate products (Lijrs/Ljrs)
-lpro_list <- lapply(ljrs_list, function(x){ x / colSums(x) })
-lpro_df_list <- do.call(rbind, lpro_list)
-lpro <- as.matrix(as.data.frame(lapply(lpro_df_list, as.numeric)))
-lpro[is.nan(lpro)] <- 0
+lpro <- L %*% ginv(ljrs)
 
-L_1986 <- t(lpro %*% t(lsup)) %*% t(llev) #error: non-conformable arguments
-L_1986 <- lpro %*% t(lsup)
+L_1986 <- lpro %*% lsup %*% llev
 
--------------------------------
+#-------------------------------
 # built Y variables
--------------------------------
+#-------------------------------
   
 # select columns with final demand for food only
 str(Y)
 Y_df_Food <- as.data.frame(Y) %>% 
-  select(contains("Food"))
+  dplyr::select(contains("Food"))
 Y_Food <- as.matrix(Y_df_Food)
 
 # create column sums
 ys <- colSums(Y_Food)
 
-# split Y into list of 192 data frames with 130 rows each
-yrs_list <- split.data.frame(Y_df_Food, rep(1:192, each = 130))
+# split Y into list of 10 data frames with 130 rows each
+yrs_list <- split.data.frame(Y_df_Food, rep(1:10, each = 130))
 
 # sum up columns of each data frame
 yrs_list_sum <- lapply(yrs_list, FUN = colSums)
@@ -70,33 +81,29 @@ yrs <- do.call(rbind, yrs_list_sum) %>%
   as.matrix()
 
 # get distribution of supplier countries (yrs/ys)
-# by dividing each element of a column in yrs by the corresponding element in ys
-ysup <- sweep(yrs, 2, ys, FUN = '/')
-ysup[is.nan(ysup)] <- 0
+ysup <- yrs %*% t(ginv(ys))
 
 # get distribution of products (yirs/yrs)
-# by dividing each element in Y_Food by the corresponding element in yrs
-ypro_list <- lapply(yrs_list, function(x){ x / colSums(x) })
-ypro <- do.call(rbind, ypro_list) %>% 
-  as.matrix()
-ypro[is.nan(ypro)] <- 0
+ypro <- Y_Food %*% ginv(yrs)
 
 # implement world bank data including per capita function
 source("wb_data.R")
 
 # level of final demand per GDP (ys/GDP)
-ylev <- 
+ylev <- ys %*% ginv(wb_gdp_data)
   
 # GDP per capita
 G <- 
   
 # population
 P <- 
+
+Y_1986 <- ypro %*% ysup #%*% ylev %*% G %*% P
+    
+#-------------------------------
+# finished land footprint
+#-------------------------------
   
--------------------------------
-# finished equation
--------------------------------
-  
-f <- U %*% lsup %*% lpro %*% llev %*% ysup %*% ypro %*% ylev %*% G %*% P
+F <- U %*% L_1986 #%*% Y_1986
 # * = elementwise multiplication
 # %*% = matrix multiplication
