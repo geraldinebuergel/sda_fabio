5eae27dc30ffade4364181653722b2ec287dca30
geraldinebuergel <geraldine.buergel@gmx.de>
2019-03-11 19:55:15 +0100

    change GDP

--- wb_gdp.R
+++ wb_gdp.R
@@ -24,7 +24,7 @@ gdp_current <- wb(indicator = "NY.GDP.MKTP.CD", startdate = 1986, enddate = 2013
 write.xlsx(gdp_current, "wb_current.xlsx")
 
 # eora current USD
-eora1 <- read_xlsx("Value added in production_Eora26_1970-2013 in current million USD.xlsx")
+eora1 <- read_xlsx("C:/Users/Zoe/Dropbox/Master Arbeit/excel/Value added in production_Eora26_1970-2013 in current million USD.xlsx")
 
 # sum up value added for all sectors per country
 eora2 <- slice(eora1, 1:4914) %>% 
@@ -54,7 +54,7 @@ write.xlsx(def_usa, "wb_def.xlsx")
 
 split_tibble <- function(tibble, col = 'col') tibble %>% split(., .[,col])
 
-gdp1 <- read_xlsx("fabio_GDP_constant.xlsx")
+gdp1 <- read_xlsx("C:/Users/Zoe/Dropbox/Master Arbeit/excel/fabio_GDP_constant.xlsx")
 gdp <- gdp1[, 2:5] %>% 
   split_tibble("Year") %>% 
   map(~.x[["Value"]])
