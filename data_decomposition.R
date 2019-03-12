990542c7ea63c2f0eaa80d1bf1b0442435790e23
geraldinebuergel <geraldine.buergel@gmx.de>
2019-03-12 14:29:18 +0100

    update

--- data_decomposition.R
+++ data_decomposition.R
@@ -12,21 +12,22 @@ rm(list = ls()); gc()
 # decompose L
 #-------------------------------
 
-load("L_list1.RData")
-load("L_list2.RData")
+#load("L_list.RData")
 
 # level of total input requirements (Ljs)
-llev <- lapply(L_list1, colSums) %>% 
+llev <- lapply(L_list, colSums) %>% 
   lapply(matrix, nrow = 24700, ncol = 24700, byrow = TRUE)
 
 save(llev, file = "llev_list.RData")
 
 # distribution of supplier countries (Ljrs/Ljs)
-ljrs <- lapply(L_list1, as.data.frame) %>% 
+ljrs <- lapply(L_list, as.data.frame) %>% 
   lapply(split.data.frame, rep(1:190, each = 130)) %>% 
   lapply(lapply, colSums) %>% 
   map(~ matrix(unlist(.x), nrow = 190, ncol = 24700, byrow = TRUE)) %>% 
   map(~ .x[rep(1:nrow(.x), each = 130), ])
+
+save(ljrs, file = "ljrs.RData")
 
 lsup <- map2(ljrs, llev, ~.x / .y) %>% 
   rapply(function(x) ifelse(!is.finite(x), 0, x), how = "list")
@@ -34,7 +35,7 @@ lsup <- map2(ljrs, llev, ~.x / .y) %>%
 save(lsup, file = "lsup_list.RData")
 
 # distribution of intermediate products (Lijrs/Ljrs)
-lpro <- map2(L_list1, ljrs, ~.x / .y) %>% 
+lpro <- map2(L_list, ljrs, ~.x / .y) %>% 
   rapply(function(x) ifelse(!is.finite(x), 0, x), how = "list")
 
 save(lpro, file = "lpro_list.RData")
@@ -56,13 +57,13 @@ yrs <- lapply(Y_list, split.data.frame, rep(1:190, each = 130)) %>%
   map(~ .x[rep(1:nrow(.x), each = 130), ])
 
 ysup <- map2(yrs, ys, ~.x / .y) %>% 
-  rapply(function(x) ifelse(is.na(x), 0, x), how = "list")
+  rapply(function(x) ifelse(!is.finite(x), 0, x), how = "list")
 
 save(ysup, file = "ysup_list.RData")
 
 # distribution of products (Yirs/Yrs)
 ypro <- map2(Y_list, yrs, ~.x / .y) %>% 
-  rapply(function(x) ifelse(is.na(x), 0, x), how = "list")
+  rapply(function(x) ifelse(!is.finite(x), 0, x), how = "list")
 
 save(ypro, file = "ypro_list.RData")
 
@@ -74,7 +75,7 @@ gdp <- map(gdp, ~.x[rep(1:190, each = 130)]) %>%
 
 # level of final demand per GDP (Ys/GDP)
 ylev <- map2(ys, gdp, ~.x / .y) %>% 
-  rapply(function(x) ifelse(is.na(x), 0, x), how = "list")
+  rapply(function(x) ifelse(!is.finite(x), 0, x), how = "list")
 
 save(ylev, file = "ylev_list.RData")
 
@@ -86,10 +87,10 @@ save(P, file = "P_list.RData")
 
 # GDP per capita (G)
 G <- map2(gdp, P, ~.x / .y) %>% 
-  rapply(function(x) ifelse(is.na(x), 0, x), how = "list")
+  rapply(function(x) ifelse(!is.finite(x), 0, x), how = "list")
 
 save(G, file = "G_list.RData")
 
-Y_dec2 <- pmap(list(ypro, ysup, ylev, G, P), ~..1 * ..2 * ..3 * ..4 * ..5)
-rapply(Y_dec, function(x) ifelse(is.na(x), 0, x), how = "list")
-all.equal(Y_dec2, Y_dec)
+Y_dec <- pmap(list(ypro, ysup, ylev, G, P), ~..1 * ..2 * ..3 * ..4 * ..5)
+rapply(Y_dec, function(x) ifelse(!is.finite(x), 0, x), how = "list")
+all.equal(Y_list, Y_dec)
--- sda.R
+++ sda.R
@@ -55,19 +55,30 @@ for (i in 2:3){
 }
 all.equal(F_soll[[2]], loopt[[3]][["delta_F"]])
 
+rm(L_list, ljrs); gc()
+a <- 7:9
+source("L_variables.R")
 load("U_list.RData")
+U_list <- U_list[a]
 load("lpro_list.RData")
 load("lsup_list.RData")
 load("llev_list.RData")
 load("ypro_list.RData")
+ypro <- ypro[a]
 load("ysup_list.RData")
+ysup <- ysup[a]
 load("ylev_list.RData")
+ylev <- ylev[a]
 load("G_list.RData")
+G <- G[a]
 load("P_list.RData")
+P <- P[a]
+
+avg <- function(x, y){(0.5 * x) + (0.5 * y)}
 
 # SDA function with decomposed inputs
-SDA_dec <- function(U1, U0, lpro1, lpro0, lsup1, lsup0, llev1, llev0, ypro1, ypro0, 
-                    ysup1, ysup0, ylev1, ylev0, G1, G0, P1, P0){
+SDA_dec <- function(U1, U0, lpro1, lpro0, lsup1, lsup0, llev1, llev0, 
+                    ypro1, ypro0, ysup1, ysup0, ylev1, ylev0, G1, G0, P1, P0){
   con_U <- avg((U1 - U0) %*% (lpro1 * lsup1 * llev1) %*% (ypro1 * ysup1 * ylev1 * G1 * P1), 
                (U1 - U0) %*% (lpro0 * lsup0 * llev0) %*% (ypro0 * ysup0 * ylev0 * G0 * P0))
   con_lpro <- avg(U0 %*% ((lpro1 - lpro0) * lsup1 * llev1) %*% (ypro1 * ysup1 * ylev1 * G1 * P1), 
@@ -88,30 +99,22 @@ SDA_dec <- function(U1, U0, lpro1, lpro0, lsup1, lsup0, llev1, llev0, ypro1, ypr
                U1 %*% (lpro1 * lsup1 * llev1) %*% (ypro1 * ysup1 * ylev1 * G1 * (P1 - P0)))
   delta_F <- con_U + con_lpro + con_lsup + con_llev + con_ypro + con_ysup + 
     con_ylev + con_G + con_P
-  return(list(delta_F, con_U, con_lpro, con_lsup, con_llev, con_ypro, con_ysup, 
-              con_ylev, con_G, con_P))
+  return(list(delta_F = delta_F, con_U = con_U, con_lpro = con_lpro, 
+              con_lsup = con_lsup, con_llev = con_llev, con_ypro = con_ypro, 
+              con_ysup = con_ysup, con_ylev = con_ylev, con_G = con_G, con_P = con_P))
 }
 
-#  function works, but too many NaNs...
-test_dec <- SDA_dec(U_list[[3]], U_list[[2]], 
-                    lpro[[3]], lpro[[2]], 
-                    lsup[[3]], lsup[[2]],
-                    llev[[3]], llev[[2]],
-                    ypro[[3]], ypro[[2]],
-                    ysup[[3]], ysup[[2]],
-                    ylev[[3]], ylev[[2]], 
-                    G[[3]], G[[2]],
-                    P[[3]], P[[2]])
-
-loop_dec1 <- list()
+# loop SDA with decomposed variables
+loop4 <- list()
 for (i in 2:3){
-  loop_dec1[[i]] <- SDA_dec(U_list[[i]], U_list[[(i-1)]], 
-                            lpro[[i]], lpro[[(i-1)]], 
-                            lsup[[i]], lsup[[(i-1)]],
-                            llev[[i]], llev[[(i-1)]],
-                            ypro[[i]], ypro[[(i-1)]],
-                            ysup[[i]], ysup[[(i-1)]],
-                            ylev[[i]], ylev[[(i-1)]], 
-                            G[[i]], G[[(i-1)]],
-                            P[[i]], P[[(i-1)]])
+  loop4[[i]] <- SDA_dec(U_list[[i]], U_list[[(i-1)]], 
+                        lpro[[i]], lpro[[(i-1)]], 
+                        lsup[[i]], lsup[[(i-1)]],
+                        llev[[i]], llev[[(i-1)]],
+                        ypro[[i]], ypro[[(i-1)]],
+                        ysup[[i]], ysup[[(i-1)]],
+                        ylev[[i]], ylev[[(i-1)]], 
+                        G[[i]], G[[(i-1)]],
+                        P[[i]], P[[(i-1)]])
 }
+save(loop4, file = "loop4.RData")
